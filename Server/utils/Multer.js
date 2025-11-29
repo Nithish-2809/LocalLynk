237b6f9277ae3a0023be068dfcd6275170de3fd9
Nithish Reddy <nithishreddy2809@gmail.com>
2025-11-29 23:12:29 +0530

    added .v2

--- Server/utils/Multer.js
+++ Server/utils/Multer.js
@@ -1,13 +1,15 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("./cloudinary"); 
 
+// ✅ IMPORT v2 DIRECTLY (THIS FIXES uploader undefined)
+const cloudinary = require("cloudinary").v2;
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "locallynk_products",
     allowed_formats: ["jpg", "jpeg", "png"],
+    resource_type: "auto",
   },
 });
 
