b914bcdccfc56fff68531b24636557e634fbd135
Nithish Reddy <nithishreddy2809@gmail.com>
2025-11-29 22:01:52 +0530

    fixed cors error

--- Server/Server.js
+++ Server/Server.js
@@ -17,7 +17,12 @@ const orderRouter = require("./Routes/Order");
 const DATABASE_URL = process.env.DATABASE_URL;
 
 // ========== ENABLE CORS ==========
-app.use(cors()); 
+app.use(cors({
+  origin: ["http://localhost:5174", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: false
+}));
+
 
 
 // ========== SOCKET.IO CORS ==========
