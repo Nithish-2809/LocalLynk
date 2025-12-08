101403e2951cbca68ba67de4eb26fd267257b873
Nithish Reddy <nithishreddy2809@gmail.com>
2025-12-08 19:45:28 +0530

    corrected cors error

--- Server/Server.js
+++ Server/Server.js
@@ -19,9 +19,10 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 // ========== ENABLE CORS ==========
 app.use(cors({
-  origin: ["http://localhost:5174", "http://localhost:5173"],
-  methods: ["GET", "POST","PATCH", "PUT", "DELETE"],
-  credentials: false
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 
@@ -29,9 +30,10 @@ app.use(cors({
 // ========== SOCKET.IO CORS ==========
 const io = new Server(server, {
   cors: {
-   origin: true,
-    methods: ["GET","POST","PATCH","DELETE"]
-  }
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE"]
+}
 });
 
 // =====================================
